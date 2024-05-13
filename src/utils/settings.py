bfe3b3acac69c68e1ded5e8f574ea0bc53ae0eeb
Dybedahl <adriandybdal@outlook.com>
2024-05-14 01:03:12 +0200

    Add settings module with player and screen configurations

---
+++ src/utils/settings.py
@@ -0,0 +1,14 @@
+"""
+This module contains the settings for the application. This includes the screen size, the player size, and the player speed.
+"""
+
+PLAYER_SPEED = 2
+PLAYER_WIDTH = 32
+PLAYER_HEIGHT = 32
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+ANIMATION_DELAY = 100
+# Define the player image path
+PLAYER_IMAGE_WALK_PATH = "assets/images/player/Player_Walk_Sword_Defence0_"
+PLAYER_IMAGE_ATTACK_PATH = "assets/images/player/Player_Attack_Sword_Defence0_"
