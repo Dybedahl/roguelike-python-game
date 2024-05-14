ddbbe7d56c00b3b3fe529ec494943042c3fd668f
Dybedahl <adriandybdal@outlook.com>
2024-05-14 23:27:39 +0200

    chore: Initialize game map and renderer in main function

--- src/main.py
+++ src/main.py
@@ -3,16 +3,19 @@ This is the main file for the game. It will be responsible for initializing the 
 """
 # Import the necessary modules
 import pygame
-import utils.settings as settings
+from utils import settings
 from pygame import display, event, QUIT
 from entities.player import Player
 from utils.player_movement import PlayerMovement
+from maps.map import GameMap
+from maps.map_renderer import MapRenderer
+
 
 def main():
     # Initialize the game
     init()
     screen = display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
-    display.set_caption("Rougelike Game by Dybedahl")
+    display.set_caption("Roguelike Game by Dybedahl")
     
     # Set the game tick
     clock = pygame.time.Clock()
@@ -26,7 +29,15 @@ def main():
     
     # Initialize the player movement
     player_movement = PlayerMovement(player)
-
+    
+    # Initialize the map generation
+    game_map = GameMap(settings.MAP_WIDTH, settings.MAP_HEIGHT)
+    game_map.generate_map()
+    
+    # Initialize the map renderer
+    map_renderer = MapRenderer(game_map)
+    
+    
     # Load the game
     running = True
     while running:
@@ -48,6 +59,7 @@ def main():
         
         # Draw the player on the screen
         screen.fill((255, 255, 255))  # Clear the screen
+        map_renderer.render(screen)
         screen.blit(player.image, player.rect)
         
         
