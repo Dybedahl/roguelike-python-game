3fcd6d28056580a91b4ac105049ae9cdde10c00d
Dybedahl <adriandybdal@outlook.com>
2024-05-14 23:27:26 +0200

    chore: Update player settings and add map generation settings

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -2,9 +2,9 @@
 This module contains the settings for the application. This includes the screen size, the player size, and the player speed.
 """
 
-PLAYER_SPEED = 2
-PLAYER_WIDTH = 32
-PLAYER_HEIGHT = 32
+PLAYER_SPEED = 5
+PLAYER_WIDTH = 64
+PLAYER_HEIGHT = 64
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
@@ -12,3 +12,16 @@ ANIMATION_DELAY = 100
 # Define the player image path
 PLAYER_IMAGE_WALK_PATH = "assets/images/player/Player_Walk_Sword_Defence0_"
 PLAYER_IMAGE_ATTACK_PATH = "assets/images/player/Player_Attack_Sword_Defence0_"
+
+# Define the map generation settings
+MAP_WIDTH = 800
+MAP_HEIGHT = 600
+TILE_SIZE = 32
+
+# Sprite sheet settings
+SPRITE_SHEET_PATH = "assets/images/sprites/roguelikeDungeon_transparent.png"
+SPRITE_SHEET_WIDTH = 464
+SPRITE_SHEET_HEIGHT = 288
+SPRITE_SHEET_COLUMNS = 29
+SPRITE_SHEET_ROWS = 18
+SPRITE_SHEET_SIZE = 17
